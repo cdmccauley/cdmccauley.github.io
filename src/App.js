342014222fdb462b7be33ef6d7e5e9248474a6f7
Chris McCauley <cdmccauley@gmail.com>
2018-09-10 21:07:57 -0600

    working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
+// material demo imports
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+// default imports
+// import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// material imports
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Chris McCauley
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+        </header> */}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </div>
     );
   }
