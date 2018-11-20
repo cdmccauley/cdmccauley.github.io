fd54c041c5d33f596e1205aa128f1d1a10ccdd22
Chris McCauley <cdmccauley@gmail.com>
2018-11-19 20:05:13 -0700

    adding ups-rate-live

---
+++ ups-rate-live/precache-manifest.c9447e4fc4a04164f9a9a7c590e7a541.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f74e2e6af528b5f9e58c",
+    "url": "/ups-rate-live/static/js/runtime~main.f74e2e6a.js"
+  },
+  {
+    "revision": "b7ef11ef640d0da04fb4",
+    "url": "/ups-rate-live/static/js/main.b7ef11ef.chunk.js"
+  },
+  {
+    "revision": "cd5c1d96f2253db79466",
+    "url": "/ups-rate-live/static/js/1.cd5c1d96.chunk.js"
+  },
+  {
+    "revision": "b7ef11ef640d0da04fb4",
+    "url": "/ups-rate-live/static/css/main.c208e3ff.chunk.css"
+  },
+  {
+    "revision": "2db2594e13b6b6566f5774acd9c2fa72",
+    "url": "/ups-rate-live/index.html"
+  }
+];
